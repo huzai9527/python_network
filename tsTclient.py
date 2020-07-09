4419e27cbfc8873609fbfb68192beaa8fe93a88a
huzai <369212851@qq.com>
2020-07-09 09:52:35 +0800

    new from win

--- tsTclient.py
+++ tsTclient.py
@@ -1,1 +1,20 @@
 from socket import *
+from time import ctime
+HOST = "192.168.2.103"
+PORT = 23456
+ADDR = (HOST, PORT)
+BUFSIZE = 1024
+
+tcpCliSocket = socket(AF_INET, SOCK_STREAM)
+tcpCliSocket.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSocket.send(data)
+    data = tcpCliSocket.recv(BUFSIZE)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+tcpCliSocket.close()
