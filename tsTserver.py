54e5a7b98b494ae8219ef39e3413bedd2f2f2b97
huzai_mac <369212851@qq.com>
2020-07-09 09:33:13 +0800

    new from mac

--- tsTserver.py
+++ tsTserver.py
@@ -1,1 +1,22 @@
 from socket import *
+from time import ctime
+HOST = ''
+PORT = 23456
+ADDR = (HOST, PORT)
+BUFSIZE = 1024
+
+tcpSerSocket = socket(AF_INET,SOCK_STREAM)
+tcpSerSocket.bind(ADDR)
+tcpSerSocket.listen(5)
+
+while True:
+    print("waiting for connection ...")
+    tcpCliSocket, addr = tcpSerSocket.accept()
+    print("connected from:", addr)
+    while True:
+        data = tcpCliSocket.recv(BUFSIZE)
+        if not data:
+            break
+        tcpCliSocket.send('[%s]%s'%(bytes(ctime(), 'utf-8'), data))
+    tcpCliSocket.close()
+tcpSerSocket.close()
